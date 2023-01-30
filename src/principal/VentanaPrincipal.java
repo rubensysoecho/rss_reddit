e31a3e7a403b5efa07b8dc249522d1a3c58e1682
Rubén <95583458+rubensysoecho@users.noreply.github.com>
2023-01-30 14:57:03 +0100

    Add files via upload

--- src/principal/VentanaPrincipal.java
+++ src/principal/VentanaPrincipal.java
@@ -1,29 +1,36 @@
 package principal;
 
+
 import java.awt.Desktop;
 import java.awt.EventQueue;
 import java.awt.event.MouseEvent;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumnModel;
+import javax.swing.text.html.HTMLEditorKit;
 
 import logica.GenerarPublicaciones;
 import logica.Publicacion;
 
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JEditorPane;
 
+@SuppressWarnings("serial")
 public class VentanaPrincipal extends JFrame {
 
 	private ArrayList<Publicacion> listaPublicacion = GenerarPublicaciones.devolverPublicaciones();
 
 	private JPanel contentPane;
+	private JLabel lblNewLabel;
 
 	/**
 	 * Launch the application.
@@ -61,15 +68,30 @@ public class VentanaPrincipal extends JFrame {
 	public void abrirLink(String link)	{
 		if (link.contains("http"))	{
 			Desktop desktop = java.awt.Desktop.getDesktop();
-			try	{
-				URI url = new URI(link);
+			//try	{
+				/*URI url = new URI(link);
 				desktop.browse(url);
-			} catch (URISyntaxException | IOException e)	{
+				*/
+				
+				/*try {
+					webView.setPage(link);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				
+				
+			/*} catch (URISyntaxException | IOException e)	{
 				e.printStackTrace();
-			}
+			}*/
 		}
 	}
 
+	public void mostrarTitulo(String titulo)	{
+		lblNewLabel.setText(titulo);
+	}
+	
 	public VentanaPrincipal() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1184, 721);
@@ -78,9 +100,9 @@ public class VentanaPrincipal extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-
+		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(10, 140, 1148, 531);
+		scrollPane.setBounds(10, 140, 755, 531);
 		contentPane.add(scrollPane);
 
 		String[] cols = { "Titulo", "Usuario", "Link Usuario", "Link" };
@@ -91,20 +113,53 @@ public class VentanaPrincipal extends JFrame {
 		table.setRowHeight(50);
 		table.setAutoscrolls(true);
 		table.setDefaultEditor(Object.class, null);
+		
+		TableColumnModel columnModel = table.getColumnModel();
+		columnModel.getColumn(0).setPreferredWidth(300);
+		columnModel.getColumn(1).setPreferredWidth(40);
+		columnModel.getColumn(2).setPreferredWidth(10);
+		columnModel.getColumn(3).setPreferredWidth(180);
 
 		table.addMouseListener(new java.awt.event.MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				int row = table.rowAtPoint(e.getPoint());
 				int col = table.columnAtPoint(e.getPoint());
-				String link = (String) table.getValueAt(row, col);
 				
-				if (row >= 0 && col >= 0 && e.getClickCount() == 2) {
+				String link = (String) table.getValueAt(row, col);
+				String titulo = (String) table.getValueAt(row, 0);
+				
+				mostrarTitulo(titulo);
+				
+				if (col >= 2 && e.getClickCount() == 2) {
 					abrirLink(link);
 				}
+				
 			}
 		});
 
 		scrollPane.setViewportView(table);
+		
+		lblNewLabel = new JLabel("");
+		lblNewLabel.setFont(new Font("Century Gothic", Font.BOLD, 20));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(10, 11, 755, 118);
+		contentPane.add(lblNewLabel);
+		
+		JEditorPane webView = new JEditorPane();
+		
+		try {
+			webView.setPage("https://www.google.es/");
+		} catch (IOException e1) {
+			webView.setContentType("text/html");
+			webView.setText("<html>Connection issues!</html>");
+		}
+		webView.setBounds(775, 11, 383, 660);
+		webView.setEditable(false);
+		contentPane.add(webView);
+		
+		JScrollPane scrollPane_1 = new JScrollPane(webView);
+		scrollPane_1.setBounds(775, 11, 383, 660);
+		contentPane.add(scrollPane_1);
 	}
 }
